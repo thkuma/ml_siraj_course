f3cc3fe5cbcb04fda7e89aec602e8a93a1fa5604
Thiago Kuma <thiago.kuma@loggi.com>
2019-09-11 08:06:15 -0300

    Iniciando 02

--- 02_Finance/cmd.sh
+++ 02_Finance/cmd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ENV=$(head -n 1 .conda-env)
-echo "conda create -n $ENV python			#CRIA"
-echo "conda deactivate 						#DESATIVA"
-echo "conda env create -f environment.yml 	#RECRIA"
-echo "conda env remove -y -q -n $ENV 			#APAGA"
-echo "conda env export > environment.yml 				#ATUALIZA CONFIG"
-echo "source activate $ENV 				#ATIVA"
+echo "conda create -n $ENV python"
+echo "conda deactivate"
+echo "conda env create -f environment.yml"
+echo "conda env remove -y -q -n $ENV"
+echo "conda env export > environment.yml"
+echo "source activate $ENV"
